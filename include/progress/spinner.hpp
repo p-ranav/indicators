0053107744fe4d3e8a06d13256811bb1bc474a10
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-03 16:57:18 -0600

    Not hiding cursor in spinner

--- include/progress/spinner.hpp
+++ include/progress/spinner.hpp
@@ -30,12 +30,10 @@ public:
     std::unique_lock<std::mutex> lock{_mutex};
     _progress = value / 100.0;
     float pos = _progress * static_cast<float>(_bar_width);
-    hide_cursor();
     std::cout << _states[_index % _states.size()];
     std::cout << " " << static_cast<int>(value) << "%";
     std::cout << " " << _name << "\r";
     std::cout.flush();
-    show_cursor();
     _index += 1;
   }
 };
