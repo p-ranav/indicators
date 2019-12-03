3dd8c8210520c0e916ca6d7be979ef981ae1424e
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-03 09:54:50 -0600

    Initial commit

---
+++ include/progressbar.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+class progress_bar {
+  size_t _bar_width{100};
+  std::string _start{"|"};
+  std::string _end{"|"};
+  std::mutex _mutex;
+  std::atomic<size_t> _progress{0};
+  
+public:
+
+  ~progress_bar() {
+    std::cout << std::endl;
+  }
+  
+  void increment(size_t value) {
+    std::unique_lock<std::mutex> lock{_mutex};
+    _progress = value;
+    std::cout << "[";
+    for (size_t i = 0; i < _bar_width; i++) {
+      if (i < value) std::cout << "=";
+      else if (i == value) std::cout << ">";
+      else std::cout << " ";
+    }
+    std::cout << "] " << int(_progress) << " %\r";
+    std::cout.flush();
+  }
+
+  size_t current() {
+    return _progress;
+  }
+};
