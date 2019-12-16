094f25e012141401a16e02166b714372fcf061fc
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-12-16 11:03:31 -0600

    Added progress bar samples

---
+++ samples/progress_bar_tick.cpp
@@ -0,0 +1,27 @@
+#include <indicators/progress_bar.hpp>
+#include <thread>
+#include <chrono>
+
+int main() {
+  indicators::ProgressBar bar;
+  
+  // Configure the bar
+  bar.set_bar_width(50);
+  bar.start_bar_with("[");
+  bar.fill_bar_progress_with("=");
+  bar.lead_bar_progress_with(">");
+  bar.fill_bar_remainder_with(" ");
+  bar.end_bar_with("]");
+  bar.set_postfix_text("Getting started");
+  bar.set_foreground_color(indicators::Color::GREEN); 
+  
+  // Update bar state
+  while (true) {
+    bar.tick();
+    if (bar.is_completed())
+      break;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  return 0;
+}
