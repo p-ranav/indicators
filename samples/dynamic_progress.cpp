f5c1e61676d84d99b9fcfbea90e191ef15447734
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-02-22 00:40:38 +0530

    Minor updates

--- samples/dynamic_progress.cpp
+++ samples/dynamic_progress.cpp
@@ -6,32 +6,32 @@ int main() {
 
   ProgressBar bar1{option::BarWidth{50}, option::ForegroundColor{Color::red},
                    option::ShowElapsedTime{true}, option::ShowRemainingTime{true},
-                   option::PrefixText{"    5c90d4a2d1a8: Downloading "}};
+                   option::PrefixText{"5c90d4a2d1a8: Downloading "}};
 
   ProgressBar bar2{option::BarWidth{50}, option::ForegroundColor{Color::yellow},
                    option::ShowElapsedTime{true}, option::ShowRemainingTime{true},
-                   option::PrefixText{"    22337bfd13a9: Downloading "}};
+                   option::PrefixText{"22337bfd13a9: Downloading "}};
 
   ProgressBar bar3{option::BarWidth{50}, option::ForegroundColor{Color::green},
                    option::ShowElapsedTime{true}, option::ShowRemainingTime{true},
-                   option::PrefixText{"    10f26c680a34: Downloading "}};
+                   option::PrefixText{"10f26c680a34: Downloading "}};
 
   ProgressBar bar4{option::BarWidth{50}, option::ForegroundColor{Color::white},
                    option::ShowElapsedTime{true}, option::ShowRemainingTime{true},
-                   option::PrefixText{"    6364e0d7a283: Downloading "}};
+                   option::PrefixText{"6364e0d7a283: Downloading "}};
 
   ProgressBar bar5{option::BarWidth{50}, option::ForegroundColor{Color::blue},
                    option::ShowElapsedTime{true}, option::ShowRemainingTime{true},
-                   option::PrefixText{"    ff1356ba118b: Downloading "}};
+                   option::PrefixText{"ff1356ba118b: Downloading "}};
 
   ProgressBar bar6{option::BarWidth{50}, option::ForegroundColor{Color::cyan},
                    option::ShowElapsedTime{true}, option::ShowRemainingTime{true},
-                   option::PrefixText{"    5a17453338b4: Downloading "}};
+                   option::PrefixText{"5a17453338b4: Downloading "}};
 
-  std::cout << termcolor::bold << termcolor::white << "\n\n    Pulling image foo:bar/baz\n";
+  std::cout << termcolor::bold << termcolor::white << "Pulling image foo:bar/baz\n";
 
   DynamicProgress<ProgressBar> bars(bar1, bar2, bar3);
-  bars.set_option(option::HideBarWhenComplete{false});
+  bars.set_option(option::HideBarWhenComplete{true});
 
   std::thread fourth_job, fifth_job, sixth_job;
 
@@ -39,7 +39,7 @@ int main() {
     while (true) {
       bars[i].tick();
       if (bars[i].is_completed()) {
-        bars[i].set_option(option::PrefixText{"    6364e0d7a283: Pull complete "});
+        bars[i].set_option(option::PrefixText{"6364e0d7a283: Pull complete "});
         bars[i].mark_as_completed();
         break;
       }
@@ -51,7 +51,7 @@ int main() {
     while (true) {
       bars[i].tick();
       if (bars[i].is_completed()) {
-        bars[i].set_option(option::PrefixText{"    ff1356ba118b: Pull complete "});
+        bars[i].set_option(option::PrefixText{"ff1356ba118b: Pull complete "});
         bars[i].mark_as_completed();
         break;
       }
@@ -63,7 +63,7 @@ int main() {
     while (true) {
       bars[i].tick();
       if (bars[i].is_completed()) {
-        bars[i].set_option(option::PrefixText{"    5a17453338b4: Pull complete "});
+        bars[i].set_option(option::PrefixText{"5a17453338b4: Pull complete "});
         bars[i].mark_as_completed();
         break;
       }
@@ -75,7 +75,7 @@ int main() {
     while (true) {
       bars[0].tick();
       if (bars[0].is_completed()) {
-        bars[0].set_option(option::PrefixText{"    5c90d4a2d1a8: Pull complete "});
+        bars[0].set_option(option::PrefixText{"5c90d4a2d1a8: Pull complete "});
         // bar1 is completed, adding bar6
         auto i = bars.push_back(bar6);
         sixth_job = std::thread(job6, i);
@@ -90,7 +90,7 @@ int main() {
     while (true) {
       bars[1].tick();
       if (bars[1].is_completed()) {
-        bars[1].set_option(option::PrefixText{"    22337bfd13a9: Pull complete "});
+        bars[1].set_option(option::PrefixText{"22337bfd13a9: Pull complete "});
         // bar2 is completed, adding bar5
         auto i = bars.push_back(bar5);
         fifth_job = std::thread(job5, i);
@@ -105,7 +105,7 @@ int main() {
     while (true) {
       bars[2].tick();
       if (bars[2].is_completed()) {
-        bars[2].set_option(option::PrefixText{"    10f26c680a34: Pull complete "});
+        bars[2].set_option(option::PrefixText{"10f26c680a34: Pull complete "});
         // bar3 is completed, adding bar4
         auto i = bars.push_back(bar4);
         fourth_job = std::thread(job4, i);
@@ -124,7 +124,7 @@ int main() {
   second_job.join();
   first_job.join();
 
-  std::cout << termcolor::bold << termcolor::green << "    ✔ Downloaded image foo/bar:baz\n\n\n\n" << std::endl;
+  std::cout << termcolor::bold << termcolor::green << "✔ Downloaded image foo/bar:baz" << std::endl;
   std::cout << termcolor::reset;
 
   return 0;
