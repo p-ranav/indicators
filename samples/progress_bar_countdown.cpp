728c4f7f7fb6522b57f04291e448e0fdc36060b2
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2020-05-24 23:03:39 -0500

    Added countdown progress bar sample

---
+++ samples/progress_bar_countdown.cpp
@@ -0,0 +1,35 @@
+#include <chrono>
+#include <indicators/progress_bar.hpp>
+#include <thread>
+using namespace indicators;
+
+int main() {
+
+  std::cout << "\n\n";
+
+  ProgressBar bar{option::BarWidth{50},
+                  option::ProgressType{ProgressType::decremental},
+                  option::Start{"    ["},
+                  option::Fill{"■"},
+                  option::Lead{"■"},
+                  option::Remainder{"-"},
+                  option::End{"]"},
+                  option::PostfixText{"Reverting System Restore"},
+                  option::ForegroundColor{Color::yellow},
+                  option::FontStyles{std::vector<FontStyle>{FontStyle::bold}}};
+
+  // Update bar state
+  while (true) {
+    bar.tick();
+    if (bar.is_completed())
+      break;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  std::cout << termcolor::bold << termcolor::white
+  << "    Task Failed Successfully\n" << termcolor::reset;
+
+  std::cout << "\n\n";
+
+  return 0;
+}
